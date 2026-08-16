7b57ea1cfc283d8c2d08efbbaadbee3321babff2
卄ㄚ卩乇尺几ㄖᐯ卂✨ <mhmmdeymngrbz@gmail.com>
2026-08-16 14:48:57 +0300

    Update engine version in main.cpp

--- src/windows/main.cpp
+++ src/windows/main.cpp
@@ -625,7 +625,7 @@ void print_version()
   ▀▀▀      ▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀ ▀▀▀          ▀▀▀      ▒░    ▀▀▀▀▀▀▀▀▀▀)" 
               << "\n\n";
     std::cout << "tig-pkg Package Manager — Version 0.4.4-ALPHA\n";
-    std::cout << "Engine: Dawn Package System v1.2.4-LTS\n";
+    std::cout << "Engine: hypecc Package Manager v.1.0.0-LTS\n";
     std::cout << "Licensing: GNU GPL v3.0\n";
     std::cout << "tig-pkg — Keep your vision wide, be beyond the Earth.\n";
 }
